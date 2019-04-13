b4c070b60b470c7e619a02c4ad70f9ad3584ca0e
John Lennie <johnanthonylennie@gmail.com>
2019-04-13 00:02:03 -0400

    completed all features

--- vampire.js
+++ vampire.js
@@ -10,22 +10,31 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let counter = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      counter++;
+    }
+    return counter;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return (
+      this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal
+    );
   }
 
   /** Stretch **/
@@ -36,9 +45,34 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    let currentVampire = this;
+    let comparisonVampire = vampire;
+    let currentVampireAncestors = [];
+    let comparisonVampireAncestors = [];
+    if (currentVampire === vampire) return currentVampire;
+    if (!currentVampire.creator) return currentVampire;
+    while (currentVampire.creator) {
+      currentVampireAncestors.push(currentVampire.creator);
+      currentVampire = currentVampire.creator;
+    }
+    while (comparisonVampire.creator) {
+      comparisonVampireAncestors.push(comparisonVampire.creator);
+      comparisonVampire = comparisonVampire.creator;
+    }
+    for (let i of currentVampireAncestors) {
+      if (i === vampire) {
+        return i;
+      }
+      for (var j of comparisonVampireAncestors) {
+        if (i === j) {
+          return i;
+        }
+        if (j === this) {
+          return j;
+        }
+      }
+    }
   }
 }
 
 module.exports = Vampire;
-
